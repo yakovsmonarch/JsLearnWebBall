f090f119930ca742bc84f498bdbe6fababb478ae
Яков Серебренников <serebrennikov@rbsoft.ru>
2023-05-31 23:32:06 +0800

    Реализовал свой map

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ const newBloggers = bloggers.map(function(item){
     return item.age;
 });
 console.log(newBloggers);
+
+const myMap = (arr, cllBack) => {
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        const elem = arr[i];
+        newArr.push(cllBack(elem));
+    }
+    return newArr;
+};
+
+const arrMyMap = myMap(bloggers, (p)=>{
+    return p.name;
+});
+
+console.log(arrMyMap);
