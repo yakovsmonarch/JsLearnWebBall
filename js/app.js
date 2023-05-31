255b0cf0ba268d389feeac180c3f6d2f0366f1f8
Яков Серебренников <serebrennikov@rbsoft.ru>
2023-05-31 23:59:18 +0800

    Использование filter

--- js/app.js
+++ js/app.js
@@ -1,25 +1,40 @@
-const bloggers = [
-    {name:"Alex", age: 20},
-    {name:"Vitor", age: 30},
-    {name:"Diego", age: 40},
-];
+// Создание собственного мапа.
+function CreateMap(){
+    const bloggers = [
+        {name:"Alex", age: 20},
+        {name:"Vitor", age: 30},
+        {name:"Diego", age: 40},
+    ];
+    const newBloggers = bloggers.map(function(item){
+        return item.age;
+    });
+    console.log(newBloggers);
+    
+    const myMap = (arr, cllBack) => {
+        const newArr = [];
+        for(let i = 0; i < arr.length; i++){
+            const elem = arr[i];
+            newArr.push(cllBack(elem));
+        }
+        return newArr;
+    };
+    
+    const arrMyMap = myMap(bloggers, (p)=>{
+        return p.name;
+    });
+    
+    console.log(arrMyMap);
+}
 
-const newBloggers = bloggers.map(function(item){
-    return item.age;
-});
-console.log(newBloggers);
+function useFilter(){
+    const bloggers = [
+        {id: 0, name:"Alex", age: 20},
+        {id: 1, name:"Vitor", age: 30},
+        {id: 2, name:"Diego", age: 40},
+    ];
 
-const myMap = (arr, cllBack) => {
-    const newArr = [];
-    for(let i = 0; i < arr.length; i++){
-        const elem = arr[i];
-        newArr.push(cllBack(elem));
-    }
-    return newArr;
-};
+    const newFrends = bloggers.filter((f)=>f.id > 1);
+    console.log(newFrends, " newFrends");
+}
 
-const arrMyMap = myMap(bloggers, (p)=>{
-    return p.name;
-});
-
-console.log(arrMyMap);
+useFilter();
